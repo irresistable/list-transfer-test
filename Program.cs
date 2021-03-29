3211981f17983f60bc008bd3e53bf8d5fdb913b2
Alexey Goncharov <alex.goncharoff@gmail.com>
2021-03-30 00:07:24 +0300

    Добавил комментарии

--- Program.cs
+++ Program.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 
 namespace list_transfer_test
 {
+    
+    //Делать все в рамках одного класса - не верно, так получается путаница что ты
+    //дергаешь один метод из другого
     class Program
     {
         static void Main(string[] args)
         {
-            var list = new List<int>(); //требует вызова System.Collections.Generic
+            var list = new List<int>(); //требует вызова System.Collections.Generic //не вызова, а инициализации
             AddNumbers(list); //Это не сработает, если AddNumbers будет не static, либо 
                               //класс Program не будет обьявлен явно потому, что ты обращаешься явно
                               //к статическому методу без инициализации класса
@@ -18,6 +21,9 @@ namespace list_transfer_test
 
         }
         public static void AddNumbers(List<int> numbers) // делаем метод, принимающий лист интов 
+                                                         //"принимающий", но нигде нет конструкции, которая
+                                                         //передает на вход этого метода лист интов.
+                                                         //См. строку 14
         {
             numbers.Add(1);
             numbers.Add(5);
