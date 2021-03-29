e247729ce38d35c78c31c8f4cfb44bc522a0cdd2
Alexey Goncharov <alex.goncharoff@gmail.com>
2021-03-29 23:55:26 +0300

    AddNumbers

--- Program.cs
+++ Program.cs
@@ -8,15 +8,14 @@ namespace list_transfer_test
         static void Main(string[] args)
         {
             var list = new List<int>(); //требует вызова System.Collections.Generic
-            //AddNumbers numbers = new AddNumbers(list);
-            //
+            AddNumbers(list);
             foreach (var item in list)
             {
                 Console.WriteLine(item);
             }
 
         }
-        public void AddNumbers(List<int> numbers) // делаем метод, принимающий лист интов 
+        public static void AddNumbers(List<int> numbers) // делаем метод, принимающий лист интов 
         {
             numbers.Add(1);
             numbers.Add(5);
