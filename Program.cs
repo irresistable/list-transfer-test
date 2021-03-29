5b041671c5cdc5cb3a4bb96b300380e12759dba9
Sanches <autoscript@gmail.com>
2021-03-29 21:13:16 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace list_transfer_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int>(); //требует вызова System.Collections.Generic
+            AddNumbers(list);
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+        static void AddNumbers(List<int> numbers) // делаем метод, принимающий лист интов 
+        {
+            numbers.Add(1);
+            numbers.Add(5);
+            numbers.Add(10);
+        }
+    }
+}
