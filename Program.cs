c5c5b59144d72edccc18f24ab063d39631ede98a
Sanches <autoscript@gmail.com>
2021-03-30 12:35:40 +0300

    добавил класс добавил абстрактный класс

--- Program.cs
+++ Program.cs
@@ -7,19 +7,42 @@ namespace list_transfer_test
     {
         static void Main(string[] args)
         {
-            var list = new List<int>(); //требует вызова System.Collections.Generic
-            AddNumbers(list);
+            var list = new List<int>(); //требует вызова System.Collections.Generic //list ссылается на память для экземпляра листа
+            Addition testex = new Addition();
+            testex.AddNumbers(list);
+            AddNumbers_static(list);
+            //Addition_abstract(list); не работает так
+
             foreach (var item in list)
             {
                 Console.WriteLine(item);
             }
 
         }
-        static void AddNumbers(List<int> numbers) // делаем метод, принимающий лист интов 
+        public class Addition
         {
-            numbers.Add(1);
-            numbers.Add(5);
-            numbers.Add(10);
+            public void AddNumbers(List<int> numbers) // делаем метод, принимающий лист интов.  numbers ссылается на list
+            {
+                numbers.Add(1);
+                numbers.Add(5);
+                numbers.Add(10);
+            }
         }
+        abstract class Addition_abstract
+        {
+            public void AddInAbstract(List<int> numbers) // делаем метод, принимающий лист интов.  numbers ссылается на list
+            {
+                numbers.Add(1);
+                numbers.Add(5);
+                numbers.Add(10);
+            }
+        }
+        static void AddNumbers_static(List<int> numbers) // делаем метод, принимающий лист интов.  numbers ссылается на list
+            {
+                numbers.Add(1);
+                numbers.Add(5);
+                numbers.Add(10);
+            }
+        
     }
 }
