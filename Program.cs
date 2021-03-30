782cf7a1a2cf0a22671450dff53919e8e867c472
Sanches <autoscript@gmail.com>
2021-03-30 12:41:04 +0300

    1

--- Program.cs
+++ Program.cs
@@ -12,6 +12,8 @@ namespace list_transfer_test
         {
             var list = new List<int>(); //требует вызова System.Collections.Generic
             AddNumbers(list);
+            AddClass addclass = new AddClass();
+            addclass.AddNumbers(list);
             foreach (var item in list)
             {
                 Console.WriteLine(item);
@@ -24,5 +26,16 @@ namespace list_transfer_test
             numbers.Add(5);
             numbers.Add(10);
         }
+        public class AddClass
+        {
+            public void AddNumbers(List<int> numbers) // делаем метод, принимающий лист интов 
+            {
+                numbers.Add(1);
+                numbers.Add(5);
+                numbers.Add(10);
+            }
+        }
+
+
     }
 }
